737f2c7329256af7482c02518ba9fefced508a77
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-16 16:57:08 +0800

    intent working from StudentMenu to quiz pages

--- app/src/main/java/com/demo/quizmasterapp/StudentMenu.java
+++ app/src/main/java/com/demo/quizmasterapp/StudentMenu.java
@@ -2,13 +2,34 @@ package com.demo.quizmasterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentMenu extends AppCompatActivity {
+
+    Button quizbtn, gradesbtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_menu);
+
+
+        quizbtn = (Button) findViewById(R.id.quizbtn);
+        gradesbtn = (Button) findViewById(R.id.gradesbtn);
+
+        quizbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(StudentMenu.this, QuizPage.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
