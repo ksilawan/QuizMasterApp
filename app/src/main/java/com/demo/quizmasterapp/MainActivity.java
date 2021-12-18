816e72ff44cf7f3a00f59ee627d016f6cc84c00e
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-18 15:27:24 +0800

    intent on class list to input grade

--- app/src/main/java/com/demo/quizmasterapp/MainActivity.java
+++ app/src/main/java/com/demo/quizmasterapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.demo.quizmasterapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnupdate1
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
