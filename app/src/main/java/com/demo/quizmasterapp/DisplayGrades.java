b93359d85d44e1e05aa6de2e2fe8a98ceab951bc
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-18 18:16:32 +0800

    grades formula

--- app/src/main/java/com/demo/quizmasterapp/DisplayGrades.java
+++ app/src/main/java/com/demo/quizmasterapp/DisplayGrades.java
@@ -3,12 +3,29 @@ package com.demo.quizmasterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DisplayGrades extends AppCompatActivity {
+
+    TextView finalGrade;
+    EditText engageInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_grades);
+
+        finalGrade = (TextView) findViewById(R.id.finalGrade);
+
+        float engage = Float.parseFloat(engageInput.getText().toString());
+        float engageFinal = engage - (engage * 0.10f);
+
+        finalGrade.setText("FINAL GRADE: " + engageFinal);
+
+
     }
 }
--- app/src/main/java/com/demo/quizmasterapp/InputGrades.java
+++ app/src/main/java/com/demo/quizmasterapp/InputGrades.java
@@ -6,10 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class InputGrades extends AppCompatActivity {
 
         Button computebtn;
+        EditText engageInput;
+        EditText explainInput;
+        EditText elabInput;
+        EditText exploreInput;
+        EditText evaluateInput;
+        TextView finalGrade;
 
 
     @Override
@@ -18,6 +26,12 @@ public class InputGrades extends AppCompatActivity {
         setContentView(R.layout.activity_input_grades);
 
         computebtn = (Button) findViewById(R.id.computebtn);
+        engageInput = (EditText) findViewById(R.id.engageInput);
+        explainInput = (EditText) findViewById(R.id.explainInput);
+        elabInput = (EditText) findViewById(R.id.elabInput);
+        exploreInput = (EditText) findViewById(R.id.exploreInput);
+        evaluateInput = (EditText) findViewById(R.id.evaluateInput);
+
 
         computebtn.setOnClickListener(new View.OnClickListener() {
             @Override
