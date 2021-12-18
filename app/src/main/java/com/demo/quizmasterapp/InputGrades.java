352ce3ea0a905fb97f280e3abbc97b6220213ad9
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-18 16:04:53 +0800

    added display grades page

--- app/src/main/java/com/demo/quizmasterapp/InputGrades.java
+++ app/src/main/java/com/demo/quizmasterapp/InputGrades.java
@@ -2,13 +2,31 @@ package com.demo.quizmasterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InputGrades extends AppCompatActivity {
+
+        Button computebtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_grades);
+
+        computebtn = (Button) findViewById(R.id.computebtn);
+
+        computebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(InputGrades.this, DisplayGrades.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/demo/quizmasterapp/MainActivity.java
+++ app/src/main/java/com/demo/quizmasterapp/MainActivity.java
@@ -1,20 +1,32 @@
 package com.demo.quizmasterapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnupdate1
+    Button btnupdate1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnupdate1 = (Button) findViewById(R.id.btnupdate1);
 
+        btnupdate1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, InputGrades.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
