7c181fb14caa4b33de7dc8be65669e3bf990a2f3
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-30 00:26:25 +0800

    new updates

--- app/src/main/java/com/demo/quizmasterapp/QuestionModel.java
+++
@@ -1,4 +0,0 @@
-package com.demo.quizmasterapp;
-
-public class QuestionModel {
-}
--- app/src/main/java/com/demo/quizmasterapp/StudentMainMenu.java
+++ app/src/main/java/com/demo/quizmasterapp/StudentMainMenu.java
@@ -2,13 +2,20 @@ package com.demo.quizmasterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentMainMenu extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main_menu);
+
+
+
     }
 }
