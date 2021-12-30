1aede97941df9d673dd9cf5b127b4195b185a450
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-30 18:18:41 +0800

    updates

--- app/src/main/java/com/demo/quizmasterapp/StudentGradesDisplayPage.java
+++ app/src/main/java/com/demo/quizmasterapp/StudentGradesDisplayPage.java
@@ -2,13 +2,35 @@ package com.demo.quizmasterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentGradesDisplayPage extends AppCompatActivity {
+
+    Button btnback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_grades_display_page);
+
+        btnback = findViewById(R.id.btnback);
+
+
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(StudentGradesDisplayPage.this, StudentMainMenu.class);
+                startActivity(intent);
+
+
+            }
+
+
+        });
+
     }
 }
--- app/src/main/java/com/demo/quizmasterapp/StudentMainMenu.java
+++ app/src/main/java/com/demo/quizmasterapp/StudentMainMenu.java
@@ -9,12 +9,27 @@ import android.widget.Button;
 
 public class StudentMainMenu extends AppCompatActivity {
 
+    Button btngrades;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main_menu);
+
+        btngrades = findViewById(R.id.btngrades);
+
+
+        btngrades.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(StudentMainMenu.this, StudentGradesDisplayPage.class);
+                startActivity(intent);
+            }
+
+
+        });
 
     }
 }
