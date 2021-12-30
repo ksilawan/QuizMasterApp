d0fa01af9ac2295c754d2c06e9ff3ef011edfd38
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-30 15:28:17 +0800

    added studentdisplay page

---
+++ app/src/main/java/com/demo/quizmasterapp/StudentGradesDisplayPage.java
@@ -0,0 +1,14 @@
+package com.demo.quizmasterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentGradesDisplayPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_grades_display_page);
+    }
+}
