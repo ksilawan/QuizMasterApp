44416bcad7dd7d1e580c20a23457f5bf51e39381
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-30 18:36:48 +0800

    added teacher main menu and teacher login

---
+++ app/src/main/java/com/demo/quizmasterapp/TeacherLogIn.java
@@ -0,0 +1,14 @@
+package com.demo.quizmasterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_log_in);
+    }
+}
---
+++ app/src/main/java/com/demo/quizmasterapp/TeacherMainMenu.java
@@ -0,0 +1,14 @@
+package com.demo.quizmasterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_main_menu);
+    }
+}
