2a1af8cdfbc05f9b3a86b11856e87635c0de7230
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-30 13:47:45 +0800

    update

--- app/src/main/java/com/demo/quizmasterapp/StudentMainMenu.java
+++ app/src/main/java/com/demo/quizmasterapp/StudentMainMenu.java
@@ -9,29 +9,12 @@ import android.widget.Button;
 
 public class StudentMainMenu extends AppCompatActivity {
 
-    Button btngetquiz;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main_menu);
-
-        btngetquiz = findViewById(R.id.btngetquiz);
-
-
-        btngetquiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(StudentMainMenu.this, QuizPage.class);
-                startActivity(intent);
-
-
-            }
-
-
-        });
 
     }
 }
